c33e92f3de9f8232cd57050d990eb9d747982a38
Jeffherson Vega <jeffhersonlim@gmail.com>
2017-08-05 10:05:09 -0400

    Start

---
+++ Proto-Platform/Assets/Scripts/EntryPortalScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntryPortalScript : MonoBehaviour {
+    public GameObject exit;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        other.transform.position = exit.transform.position;
+    }
+
+}
